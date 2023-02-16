75d99f708aa9e539825c65cd67dc180ef088c35b
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-16 15:00:24 -0800

    puts correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - printing with the puts function
- * Return - (0)
+ * Return - 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
