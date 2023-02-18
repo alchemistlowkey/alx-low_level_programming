e48684dc5f9e0b42ceed0cd1a43df646d0f389fd
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-17 20:29:07 -0800

    lower

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - all characters to lower case in reverse
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+	int l;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+	{
+		l = tolower(ch);
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
