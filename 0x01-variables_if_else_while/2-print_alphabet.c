a7c2211a649644af199a04f44379dcf8b6ba7da0
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-17 16:43:47 -0800

    lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - all characters to lower case
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c = tolower;
+
+	putchar(c);
+	return (0);
+}
