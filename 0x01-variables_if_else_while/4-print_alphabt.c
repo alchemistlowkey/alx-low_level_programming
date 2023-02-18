0ff068e4eaafa44f9fb4b32ea72ccb7f5330873a
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-17 19:21:14 -0800

    exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - all characters to lower case
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+	int l;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		l = tolower(ch);
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
