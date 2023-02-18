f1f74c8be1f469565babcc78f8fe971e9c804a72
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-17 19:47:12 -0800

    putchar num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - less than 10 numbers
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
