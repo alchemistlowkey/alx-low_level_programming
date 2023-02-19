8f16d1deaca32fc9d3eef6ce17745ef172d5c03e
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-19 13:05:19 -0800

    by 3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,18 +12,24 @@ int main(void)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 48; i < 58; i++)
 	{
 		for (j = 48; j < 58; j++)
 		{
-			if (i < 57)
-			if (j < 57)
-			putchar(i);
-			putchar(j);
+			for (k = 48; k < 58; k++)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i < 57)
+				if (j < 57)
+				if (k < 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
