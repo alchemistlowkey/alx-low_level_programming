465ad3ec4eff159779cdc0ad31de27c3b90ddb16
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-19 10:40:34 -0800

    possible combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,14 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 478; i <= 488; i++)
+	for (i = 48; i < 58; i++)
 	{
-		for (j = 478; i <= 488; j++)
+		for (j = 48; i < 58; j++)
 		{
 			putchar(i);
+			if (i != 57)
 			putchar(j);
+			else if (j != 57)
 			{
 				putchar(',');
 				putchar(' ');
