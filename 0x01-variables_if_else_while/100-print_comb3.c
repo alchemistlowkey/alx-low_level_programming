beb901646ce44aee1c1f4453c500aabaf35e50ec
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-20 02:39:45 -0800

    doubles

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,18 +13,17 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i <= 56; i++)
 	{
-		for (j = 48; j < 58; j++)
+		for (j = 49; j <= 57; j++)
 		{
 			if (i != j)
 			if (j != i)
 			if (i < j)
-			if (i && j)
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
 			{
-				putchar(i);
-				putchar(j);
-				if (i <= 56 && j <= 57)
 				putchar(',');
 				putchar(' ');
 			}
