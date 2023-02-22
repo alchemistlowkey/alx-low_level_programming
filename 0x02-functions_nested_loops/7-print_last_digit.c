4f1e454860d3d9ed538ad05cc8fb074e8894cf29
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-21 19:59:34 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,19 @@
 /**
  * print_last_digit - A function that prints the last digit of a number
  * @i: last digit
- * Return: i
+ * Return: j
  */
 int print_last_digit(int i)
 {
-	for (i = 0; i < 10; i++)
-	{
-		_putchar((i % 10) + '0');
-	}
-	return (i);
+	int j;
+
+	if (n < 0)
+		n = -n;
+
+	j = i % 10;
+
+	if (j < 0)
+		j = -j;
+	_putchar(j + '0');
+	return (j);
 }
