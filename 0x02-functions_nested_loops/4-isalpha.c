21985fefa7cb9c923c5524e365e871131faef082
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-21 18:41:35 -0800

    alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,9 +6,7 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90)
-		return (1);
-	else if (c >= 97 && <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && <= 122))
 		return (1);
 	else
 		return (0);
