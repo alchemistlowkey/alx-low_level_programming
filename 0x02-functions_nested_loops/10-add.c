a0e5d7765cbe6109ca7527b199400f50f21a0d10
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-21 21:27:48 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - check the code
+ * @i: first integer
+ * @j: second integer
+ * Return: Always 0.
+ */
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+	_putchar(k);
+	return (0);
+}
