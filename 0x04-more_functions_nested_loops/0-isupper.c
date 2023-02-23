5187a534447c7db6e8761d138da84f10c42ae083
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-23 11:31:54 -0800

    upper character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * _isupper - A function that checks for upper case
- *
+ * @c: integer
  * Return: 1 (success), 0 (otherwise)
  */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
-		printf("%d", c);
 		return (1);
 	}
 	else
