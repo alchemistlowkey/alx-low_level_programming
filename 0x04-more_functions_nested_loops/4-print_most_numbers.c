49ff68f295bcbcc6e33c1509da2466cd5b3f96ef
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-24 15:07:45 -0800

    2 and 4 exception

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - form 0 - 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 2)
+		if (i != 4)
+		_putchar(i);
+	}
+		_putchar('\n');
+}
