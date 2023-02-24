c5df2b12927242bada9f1204104635093037554a
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-24 15:12:02 -0800

    2 and 4 exception

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - form 0 - 9
+ * print_most_numbers - from 0 - 9 except 2 and 4
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
