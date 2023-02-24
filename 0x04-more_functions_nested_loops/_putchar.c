77c1580f20685c93a1fb5722c81c9633ed2f17c0
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-24 13:56:51 -0800

    upper case check

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -3,9 +3,10 @@
 #include <unistd.h>
 /**
  * _putchar - A function that prints to stdout
+ * @c: character
  * Return: success
  */
-int _putchar(int)
+int _putchar(char c)
 {
-	return (0);
+	return (write(1, &c, 1));
 }
