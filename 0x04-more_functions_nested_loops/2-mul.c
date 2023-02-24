bdb1a92de49bdb0cca59b271f074a621198ae846
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-24 14:22:34 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply
+ * @a: integer
+ * @b: integer
+ * Return: c (product of a and b)
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
