82fe2e217e438420e564419c4d83136103aec850
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-27 13:39:29 -0800

    swapped

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - to swap a and b
+ * @a: integer a swap
+ * @b: integer b swap
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
