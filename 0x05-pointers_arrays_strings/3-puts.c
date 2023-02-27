862a70317c1f71f4281f8a2f54685a54089ba688
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-27 15:39:03 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function to print a string
+ * @str: string
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i++;)
+	{
+		if (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+}
