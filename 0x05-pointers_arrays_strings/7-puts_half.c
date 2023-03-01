d4b63376ef24496f4202fc05d090e85fc8b51c99
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-28 18:33:48 -0800

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - A function that prints everyother character
+ * @str: character
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = i / 2)
+		{
+			if (strlen(str) < j)
+			{
+				continue;
+			}
+		}
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
