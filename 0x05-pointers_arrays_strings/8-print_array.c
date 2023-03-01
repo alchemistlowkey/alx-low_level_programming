ee877c15a31c31cd7fdd247bbff833ee5ca8b63d
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-28 19:11:48 -0800

    nth

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 /**
  * print_array - A function that prints nth element
- * @str: character
+ * @a: integer
+ * @n: integer
  * Return: Always 0.
  */
 void print_array(int *a, int n)
