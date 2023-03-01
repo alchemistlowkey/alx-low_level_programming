f723552366534119026a20f0827de9c1af806943
alchemistlowkey <alchemistlowkey@gmail.com>
2023-02-28 18:09:56 -0800

    everyother

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - A function that prints everyother character
+ * @str: character
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 1 )
+		{
+			continue;
+		}
+		_putchar(str[i]);
+	}
+}
