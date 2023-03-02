a571143486259251ab7b2e1b209a03377dad678a
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-02 14:06:46 -0800

    concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcat - concate strings
+ * @dest: character
+ * @src: character
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, l1, l2;
+
+	l1 = strlen(dest);
+	l2 = strlen(src);
+	for (i = 0; i <= l2; i++)
+	{
+		dest[l1 + i] = src[i];
+	}
+}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * _putchar - A function that prints to stdout
+ * Return: success
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
