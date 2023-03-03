b144a3f4a16996635e786373d88699c0732d616b
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-02 17:38:14 -0800

    lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *string_toupper - change to upper case
+ *
+ * Return: Always 0.
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; n(i) != '\0'; i++)
+	{
+		if (n[i] >= 97 && n[i] <= 122)
+		{
+			n[i] = n[i] - 32;
+		}
+	}
+	return (0);
+}
