3951ae2d9b8a5428705f85b46e9fe9f0120cc96b
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-03 17:19:11 -0800

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,9 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i != '\n'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		if (n == i)
+		if (i < n)
 		{
 			dest[i] = src[n];
 		}
