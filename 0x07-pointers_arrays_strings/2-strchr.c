e80b8a26d805c42dc4442a633ff5f412bb71f5c1
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-06 14:08:18 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strchr - string search
+ * @s: pointer
+ * @c: character to search
+ * Return: Nothing.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
