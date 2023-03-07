7dba9c802b390970edc9b000203f02444be470bb
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-07 13:49:10 -0800

    diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,12 +3,27 @@
 #include <string.h>
 
 /**
- * print_diagsums - chessboard
+ * print_diagsums - diagonal print
  * @a: integer
  * @size: integer
  * Return: Nothing.
  */
 void print_diagsums(int *a, int size)
 {
-	
+	int i, j, k;
+
+	k = 0;
+	j = 0;
+	for (i = 0; i < (size * size); i++)
+	{
+			if (i % (size +1) == 0)
+			{
+				k = k + *(a + i);
+			}
+			if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			{
+				j = j + *(a + i);
+			}
+	}
+	printf("%d, %d\n", k, j);
 }
