dae43ee087d7295eba65b0f76d4bfc0aaf25c7ac
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-06 21:11:11 -0800

    chess

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_diagsums - chessboard
+ * @a: integer
+ * @size: integer
+ * Return: Nothing.
+ */
+void print_diagsums(int *a, int size)
+{
+	
+}
