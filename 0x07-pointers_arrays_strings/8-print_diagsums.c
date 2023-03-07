0faa3863dac297a7df4cf47003e24c21c47c96c8
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-07 13:52:03 -0800

    diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,14 +16,14 @@ void print_diagsums(int *a, int size)
 	j = 0;
 	for (i = 0; i < (size * size); i++)
 	{
-			if (i % (size +1) == 0)
-			{
-				k = k + *(a + i);
-			}
-			if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-			{
-				j = j + *(a + i);
-			}
+		if (i % (size + 1) == 0)
+		{
+			k = k + *(a + i);
+		}
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+		{
+			j = j + *(a + i);
+		}
 	}
 	printf("%d, %d\n", k, j);
 }
