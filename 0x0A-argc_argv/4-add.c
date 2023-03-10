15c26f20e3bc453be026c23db9bb749821d7b68f
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-10 13:12:12 -0800

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program name
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, add = 0;
+
+	if (argc < 1)
+	{
+		printf("0\n");
+	}
+	if (argc == 101)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			add += atoi(argv[i]);
+		}
+		printf("%d\n", add);
+	}
+	return (0);
+}
