2f9859a42dfad3678fc8c55176e84da457ebe6f2
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-10 09:50:35 -0800

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program name
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("\"%s\".\n", argv[0]);
+	return (0);
+}
