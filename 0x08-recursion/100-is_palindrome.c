4e02491fbb2808cb28faf09f86d29646d19a748b
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-09 16:53:44 -0800

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,19 +3,47 @@
 #include <string.h>
 
 /**
+ * _strlen - string length
+ * @s: character
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen(s + 1));
+	}
+}
+
+/**
+ * _compare = compare string
+ * @s: character
+ * @i: integer
+ * @j: integer
+ * Return: 0
+ */
+int _compare(char *s, int i, int j)
+{
+	if (*(s + i) == *(s + j))
+	{
+		if (i == j || i == j + 1)
+			return (1);
+		return (0 + _compare(s, i + 1, j - 1));
+	}
+	return (0);
+}
+
+/**
  * is_palindrome - Palindrome check
  * @s: character
  * Return: Nothing.
  */
 int is_palindrome(char *s)
 {
-	int i = 0, j = 0, len;
-
-	len = strlen(s);
 	if (*s == '\0')
-		return (0);
-	if (*(s + i) == *(s + j))
-	if (i == j + 1 || i == j)
 		return (1);
-	return (len - 1);
+	return (_compare(s, 0, _strlen(s) - 1));
 }
