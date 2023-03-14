c600fbe972cf953d94c3dab9130489e184a1b09b
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-14 15:31:00 -0700

    string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,6 +4,38 @@
 #include <string.h>
 
 /**
+ * _strlen - length of a string
+ * @s: character
+ * Return: s
+ */
+int _strlen(char *s)
+{
+        int i = 0;
+
+        for (i = 0; s[i] != '\0';)
+                i++;
+        return (i);
+}
+
+/**
+ * *_strncpy - copy strings
+ * @dest: character
+ * @src: character
+ * Return: dest character
+ */
+char *_strcpy(char *dest, char *src)
+{
+        int i;
+
+        for (i = 0; src[i] != '\0'; i++)
+        {
+                dest[i] = src[i];
+        }
+        dest[i] = '\0';
+        return (dest);
+}
+
+/**
  * *_strdup - pointer return
  * @str: character
  * Return: Nothing.
