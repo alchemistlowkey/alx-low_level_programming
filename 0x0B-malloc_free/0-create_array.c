aa2933b7844a510a2738b026924eb6c4674f148b
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-14 14:11:25 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	buffer = (char*) malloc(size * sizeof(c));
+	buffer = (char *) malloc(size * sizeof(c));
 	if (buffer == 0)
 	{
 		return (NULL);
