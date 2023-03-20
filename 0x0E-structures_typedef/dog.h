9d8766c0fccc06013de3933b5e1120eb45a442a4
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-20 15:58:02 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,19 @@
+#ifndef dog_h
+#define dog_h
 #include <stdio.h>
+
+/**
+ * struct dog - data type
+ * @name: character 1st member
+ * @age: float 2nd member
+ * @owner: character 3rd member
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
