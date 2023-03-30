2049b4f5161a4b63ec9d04a5b9e72a56284dc795
alchemistlowkey <alchemistlowkey@gmail.com>
2023-03-30 15:03:06 -0700

    singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - A function to print all the elements in a list
+ * @h: const pointer to the list
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 1; h->next != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	printf("[%d] %s\n", h->len, h->str);
+	return(i);
+}
---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,10 @@
+#include "list.h"
+
+/**
+ * _putchar - A function that prints to stdout
+ * Return: success
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
