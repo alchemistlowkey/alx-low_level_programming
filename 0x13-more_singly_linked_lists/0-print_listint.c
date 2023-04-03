d9a90a8288f5c21584e667cf1ff0e69811d284da
alchemistlowkey <alchemistlowkey@gmail.com>
2023-04-03 15:37:10 -0700

    prints all elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint - Function that prints all the elements of a list
+ * @h: const listint_t
+ * Return: i(number of nodes)
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	for (i = 1; h -> next != NULL; i++)
+	{
+		if (h -> n != 0)
+		{
+			printf("%d\n", h -> n);
+		}
+		h = h -> next;
+	}
+	printf("%d\n", h -> n);
+	return(i);
+}
