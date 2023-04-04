0f9c0bf60e84f74a6e327f4479ee81e3385ee43e
alchemistlowkey <alchemistlowkey@gmail.com>
2023-04-04 16:09:24 -0700

    insertion of node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,26 +21,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = tmp->next;
 	tmp->next = new_node;
+	if (new_node == NULL)
+	{
+	return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		new_node->n = n;
+		return (new_node);
+	}
 	for (i = 1; i < idx; i++)
 	{
 		tmp = tmp->next;
-	}
-	if (new_node == NULL)
-	{
-		return (NULL);
 	}
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		new_node->n = n;
 		*head = new_node;
-		return (new_node);
-	}
-	if (*head == NULL)
-	{
-		*head = new_node;
-		new_node->n = n;
-		new_node->next = NULL;
 		return (new_node);
 	}
 	return (new_node);
