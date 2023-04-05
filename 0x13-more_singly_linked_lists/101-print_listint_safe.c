eee279497a6bc51f3651a69a1243dddbec44dfdd
alchemistlowkey <alchemistlowkey@gmail.com>
2023-04-05 03:55:09 -0700

    print list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a listint_t linked list
+ * @head: const listint_t
+ * Return: i
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *new, *tmp;
+	size_t i;
+
+	if (head == NULL)
+	{
+		exit(98);
+	}
+	new = head;
+	tmp = head;
+	for (i = 0; new && tmp && tmp->next && head != NULL; i++)
+	{
+		new = new->next;
+		tmp = tmp->next->next;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		if (new == tmp)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			exit(98);
+		}
+	}
+	head = NULL;
+	return (i);
+}
