1d3c80bc1c7daaba198b1e9db5622194227135e3
alchemistlowkey <alchemistlowkey@gmail.com>
2023-04-09 02:03:34 -0700

    bit enabling

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int ref;
 
 	ref = 0x01;
-	ref <<= index;
+	ref = ref << index;
 	if (ref == 0)
 	{
 		return (-1);
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,11 +11,11 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int ref;
 
 	ref = 0x01;
-	ref <<= index;
+	ref = ref << index;
 	if (ref == 0)
 	{
 		return (-1);
 	}
-	*n |= ref;
+	*n = *n | ref;
 	return (1);
 }
