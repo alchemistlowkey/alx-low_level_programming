2608cd83c1e7f71a1e13ec5a8895d76f91aa9c26
alchemistlowkey <alchemistlowkey@gmail.com>
2023-04-10 10:55:20 -0700

    read and print to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - A function that reads a text file and prints to stdout
+ * @filename: const character
+ * @letters: size_t
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *str;
+	ssize_t i, j, k;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	str = malloc(sizeof(char) * letters);
+	if (str == NULL)
+	{
+		return (0);
+	}
+	k = open(filename, O_RDONLY);
+	j = read(k, str, letters);
+	i = write(STDOUT_FILENO, str, j);
+	free(str);
+	close(k);
+	return (i);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,9 @@
+File Handling in C
+File I/O
+
+Open, close, read, write, dprintf
+Oflags:- O_RDONLY, O_WRONLY, O_RDWR, O_CREAT, O_APPEND
+Standard file descriptors
+0 - Standard Input - stdin - STDIN_FILENO
+1 - Standard Output - stdout - STDOUT_FILENO
+2 - Standard Error - stderr - STDERR_FILENO
