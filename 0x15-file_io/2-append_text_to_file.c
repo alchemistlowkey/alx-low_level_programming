976aea6a1930eee7942326bb2c03602c2fc59ce2
alchemistlowkey <alchemistlowkey@gmail.com>
2023-04-10 14:43:18 -0700

    append correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	k = open(filename, O_RDWR | O_APPEND);
-	for (i = 0; (*text_content + i);)
+	for (i = 0; *(text_content + i);)
 	{
 		i++;
 	}
