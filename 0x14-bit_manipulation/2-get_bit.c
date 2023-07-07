a85fe72fd12b8d781daf6ae3e30a9a14016f6316
alchemistlowkey <alchemistlowkey@gmail.com>
2023-07-07 14:09:25 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,20 +8,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int ref;
-
-	ref = 0x01;
-	ref = ref << index;
-	if (ref == 0)
+	if (index >= sizeof(n) * 8)
 	{
 		return (-1);
 	}
-	if (n & ref)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return ((n >> index) & 1);
 }
