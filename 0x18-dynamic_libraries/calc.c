6eeae80003e6ce2a012ac5e20e756c7a3eb8e4a0
alchemistlowkey <alchemistlowkey@gmail.com>
2023-08-21 08:16:30 -0700

    c and python

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ * @i: first integer
+ * @j: second integer
+ * Return: k
+ */
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+	return (k);
+}
+
+/**
+ * sub - subtraction
+ * @a: integer
+ * @b: integer
+ * Return: c (difference of a and b)
+ */
+int sub(int a, int b)
+{
+	int c;
+
+	c = a - b;
+	return (c);
+}
+
+/**
+ * mul - multiply
+ * @a: integer
+ * @b: integer
+ * Return: c (product of a and b)
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+
+/**
+ * div - division
+ * @a: integer
+ * @b: integer
+ * Return: c (division of a and b)
+ */
+int div(int a, int b)
+{
+	int c;
+
+	c = a / b;
+	return (c);
+}
+
+/**
+ * mod - modulus
+ * @a: integer
+ * @b: integer
+ * Return: c (remainder of a and b)
+ */
+int mod(int a, int b)
+{
+	int c;
+
+	c = a % b;
+	return (c);
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -2,7 +2,6 @@
 #define main_h
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 int _putchar(char c);
