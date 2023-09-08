1fff5a7bd22cfc5890a2590e419201081163375b
alchemistlowkey <alchemistlowkey@gmail.com>
2023-09-08 16:16:07 -0700

    hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - A function that creates hash table.
+ * @size: Hash table size
+ * Return: A pointer to the hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *new = NULL;
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new->array)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		(new->array)[i] = NULL;
+	}
+	return (new);
+}
