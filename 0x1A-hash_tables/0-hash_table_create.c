d4edeac4ec77da18c80194259cc8c82a2b093bc3
alchemistlowkey <alchemistlowkey@gmail.com>
2023-09-08 16:53:31 -0700

    djb2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,24 +10,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *new = NULL;
 
-	if (size < 1)
-		return (NULL);
-
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
-
 	new->size = size;
-	new->array = malloc(sizeof(hash_table_t *) * size);
-
-	if (new->array == NULL)
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new->array)
 	{
 		free(new);
 		return (NULL);
 	}
-
 	for (i = 0; i < size; i++)
 	{
 		new->array[i] = NULL;
