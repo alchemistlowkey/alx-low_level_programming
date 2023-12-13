feeb91a9a545c0691a1da7f97d8a4a03d119126f
alchemistlowkey <alchemistlowkey@gmail.com>
2023-12-13 09:32:27 -0800

    searched

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - A function that searches for a value in a sorted array
+ * @array: A pointer to the first element of the array to search in
+ * @size: The number of elements in the array
+ * @value: The value to search for
+ * Return: The first index where value is located
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+}
